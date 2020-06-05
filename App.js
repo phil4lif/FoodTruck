5676a2df14f4c17d7a699e4a883e3db4fa2373a5
phil cowan <philipjcowan1@gmail.com>
2020-06-05 12:31:21 -0700

    test merge pr

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const instructions = Platform.select({
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.welcome}>Welcome to Food Truck</Text>
       <Text style={styles.instructions}>To get started, edit App.js</Text>
       <Text style={styles.instructions}>{instructions}</Text>
     </View>
