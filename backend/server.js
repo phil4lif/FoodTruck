15d21e54dffeab342f8c81a714da782d55839d84
phil cowan <philipjcowan1@gmail.com>
2020-06-12 18:49:59 -0700

    more server

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
-
+const routes = require('./routes')
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,7 +11,11 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
 
+//add routes
+app.use(routes);
 //connect to mongoDB
 mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/foodtruck", { useNewUrlParser: true });
 
-//Import our db routes
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+});
