754f13bb965f342988a85ad958d4a112acd6c442
phil cowan <philipjcowan1@gmail.com>
2020-06-15 18:06:19 -0700

    added button

--- client/components/UserRegForm.js
+++ client/components/UserRegForm.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Button, TouchableOpacity } from 'react-native'
 
 export default function UserRegForm() {
     return (
         <View>
             <Text>User Name</Text>
-            <TextInput placeholder = "User Name" />
+            <TextInput style={styles.input} placeholder = "User Name" />
             <Text>Email</Text>
             <TextInput placeholder = "Email" />
             <Text>Password</Text>
             <TextInput placeholder = "Password" />
             <Text>Confirm Password</Text>
             <TextInput placeholder = "Type Password Again" />
+            <TouchableOpacity style={styles.button}><Text>Register</Text></TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    input: {
+        border: '1 solid black'
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: 'blue',
+        padding: 10,
+        marginBottom: 10
+      }
+})
