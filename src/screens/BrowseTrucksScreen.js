2e1657d83590af65ec9b8321090e28d340907848
phil cowan <philipjcowan1@gmail.com>
2020-07-03 21:43:29 -0700

    adds custom unsplash image to each fake truck

--- src/screens/BrowseTrucksScreen.js
+++ src/screens/BrowseTrucksScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect }from 'react';
-import { View, Text, FlatList, Image, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, FlatList, Image, StyleSheet, ScrollView, SafeAreaView } from 'react-native';
 import TopSpacer from '../components/TopSpacer';
 import trucks from '../trucks.json';
 import TrucksList from '../components/TrucksList';
@@ -10,13 +10,15 @@ const BrowseTrucksScreen = ({ navigation }) => {
     const _id = navigation.getParam('_id')
     
     return (
+        <SafeAreaView>
         <View>
             <TopSpacer />
-            <Text>Trucks Screen</Text>
+            <Text></Text>
             <ScrollView>
-                <TrucksList results={results} title="Trucks in Your area" />
+                <TrucksList results={results} title="All Food Trucks On the Network" />
             </ScrollView>
         </View>
+        </SafeAreaView>
     )
 };
 
