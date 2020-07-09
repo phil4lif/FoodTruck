2dc6b8a5b9528edb50ba364207101f9fe9fe1aa0
phil cowan <philipjcowan1@gmail.com>
2020-07-08 17:05:31 -0700

    adds placeholder profile image, backend for getting user's favorites

--- src/api/ftn.js
+++ src/api/ftn.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
-import AsyncStorage from '@react-native-community/async-storage';
-import { navigate } from '../navigationRef';
 
 const instance = axios.create({
-  baseURL: 'http://83bf667d3023.ngrok.io',
+  baseURL: 'http://23b02efbd2e2.ngrok.io',
 });
 
 export default instance;
--- src/screens/UserHomeScreen.js
+++ src/screens/UserHomeScreen.js
@@ -1,22 +1,48 @@
 import React, { useContext } from 'react';
-import { View, StyleSheet, Text, SafeAreaView } from 'react-native';
+import { View, StyleSheet, Text, SafeAreaView, Image } from 'react-native';
 import TopSpacer from '../components/TopSpacer';
 import { FontAwesome } from '@expo/vector-icons';
+import { FlatList } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const UserHomeScreen = () => {
-  
+  const getFavorites = async () => {
+    const userid =await AsyncStorage.getItem('id')
+    const results = await ftn.get(`/api/getfavorite/${userid}`)
+  }
   return (
     <SafeAreaView>
-      <View>
-        <Text style={{ fontSize: 48 }}>Welcome to the Food Truck Network</Text>
+      <View style={styles.containerStyle}>
+        <Text style={styles.headerStyle}>Welcome to the Food Truck Network</Text>
+        <Image style={styles.profImageStyle} source={{ uri: 'https://picsum.photos/200'}} />
+        <View style={styles.faveContainerStyle}>
+          <Text>Favorites</Text>
+          <FlatList 
+          horizontal
+          showsHorizontalScrollIndicator={false}/>
+        </View>
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  faveContainerStyle:{
+    borderColor: 'red',
+    borderWidth: 2,
+  },
+  containerStyle: {
+    borderColor: 'red',
+    borderWidth: 2,
+  },
   headerStyle: {
     fontSize: 36,
+  },
+  profImageStyle: {
+    height: 150,
+    width: 150,
+    borderRadius: 100,
+    alignSelf: 'center'
   },
   textStyle: {
     fontSize: 14,
