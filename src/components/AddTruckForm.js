2db64e89026557ea1a5eb64934fa40a00d6e7c4f
phil cowan <philipjcowan1@gmail.com>
2020-07-26 12:03:24 -0700

    adds text input for a photo link

--- src/components/AddTruckForm.js
+++ src/components/AddTruckForm.js
@@ -4,7 +4,8 @@ import { View, StyleSheet, Text, TouchableOpacity, Button, TextInput } from 'rea
 
 const AddTruckForm = () => {
     const [truckname, setTruckName] = useState('');
-    const [keywords, setKeyWords] = useState('')
+    const [keywords, setKeyWords] = useState('');
+    const [mainphotolink, setMainPhotoLink] = useState('')
     return(
         <View>
             <Text>Truck Name</Text>
@@ -24,6 +25,19 @@ const AddTruckForm = () => {
                 placeholder="eg. tacos, burritos, vegan, barbecue, etc"
                 value={keywords}
                 onChangeText={text => setKeyWords(text)}
+                />
+            <Text>Link a Main Photo</Text>
+            <TextInput
+                autocapitalize="none"
+                autoCorrect={false}
+                style={}
+                placeholder="Link to Main Photo"
+                value={mainphotolink}
+                onChangeText={text => setMainPhotoLink(text)}
+                />
+                <Button title="Submit"
+                style={}
+                onPress={() => onsubmit({ truckname, keywords, mainphotolink, otherphotos })}
                 />
         </View>
     )
