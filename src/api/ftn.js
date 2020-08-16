05c3e452c1052379e3e9f0d53b53db59585b0ce2
phil cowan <philipjcowan1@gmail.com>
2020-08-16 14:22:55 -0700

    pushing what I've done now

--- src/api/ftn.js
+++ src/api/ftn.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://3a6f1542e8c5.ngrok.io',
+  baseURL: 'http://86310372da28.ngrok.io',
 });
 
 export default instance;
